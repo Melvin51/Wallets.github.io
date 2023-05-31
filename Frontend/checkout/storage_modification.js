174a62695c8e981377a878cf150dbce1aa1c2676
Melvin51 <Sandwichbox42@gmail.com>
2023-05-31 21:16:59 +0300

    Initial commit

---
+++ Frontend/checkout/storage_modification.js
@@ -0,0 +1,3 @@
+localStorage.setItem("wallet_id","wallet_brown")
+console.log(localStorage.getItem("wallet_id"))
+console.log("")
--- checkout/storage_modification.js
+++
@@ -1,3 +0,0 @@
-localStorage.setItem("wallet_id","wallet_brown")
-console.log(localStorage.getItem("wallet_id"))
-console.log("Faggot")
